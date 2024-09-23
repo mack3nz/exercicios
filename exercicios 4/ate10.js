1f2b4e9fe7f4474da5a6f10ee1ad894fddc093d1
lucas <summergodsz@gmail.com>
2024-09-23 20:10:48 -0300

    Delete exercicios 4 directory

--- exercicios 4/ate10.js
+++
@@ -1,7 +0,0 @@
-// contar ate 10
-let cont = 0
-for (let i = 1; i <= 10; i = i + 1) {
-    cont++
-    console.log(cont)
-}
-
--- exercicios 4/fatorial.js
+++
@@ -1,8 +0,0 @@
-// fatorial
-prompt = require("prompt-sync")()
-var fat = Number(prompt("Digite o número: "));
-var res = fat;
-for (var i = 1; i < fat; i++) {
-    res = res * i;
-}
-console.log(res);
